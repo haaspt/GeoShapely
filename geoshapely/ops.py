73fa606704e56ee0c77750d1041422879d5adc75
Tyler Haas <patrick.tyler.haas@gmail.com>
2022-07-24 14:58:51 -0700

    Add docstring

--- geoshapely/ops.py
+++ geoshapely/ops.py
@@ -10,6 +10,40 @@ from .geoshape import GeoBaseGeometry, GeoLinearRing, GeoLineString, GeoPoint, G
 def make_geoshape_from_geom(
     shapely_geom: BaseGeometry, crs: Optional[Any] = None
 ) -> GeoBaseGeometry:
+    """Create a GeoShape instance of a Shapely geometry.
+
+    This convenience function takes in an instance of any Shapely geom and returns
+    its GeoShape equivalent. e.g.:
+        Point -> GeoPoint
+        Polygon -> GeoPolygon
+
+    Optionally, a CRS can be specified, which will be associated with the new GeoShape.
+
+    Parameters
+    ----------
+    shapely_geom:
+        An instance of any Shapely geometry that derives from BaseGeometry.
+        If a GeoShape is passed in, a copy is returned, converted to the specified
+        CRS, if one was set.
+    crs: (Optional)
+        A CRS to be associated with the new GeoShape.
+        See `pyproj.CRS.from_user_input()` for information on allowable types.
+
+    Returns
+    -------
+    A new instance of the Geo-equivalent of the Shapely geom passed in
+
+    Raises
+    ------
+    TypeError:
+        If `shapely_geom` is not derived from BaseGeometry or if no equivalent GeoShape
+        can be identified.
+    """
+    if not isinstance(shapely_geom, BaseGeometry):
+        raise TypeError(
+            f"`shapely_geom` must be an instance of Shapely geometry \
+            derived from `BaseGeometry`, cannot convert value of type {type(shapely_geom)}"
+        )
     if isinstance(shapely_geom, GeoBaseGeometry):
         # Just create a copy if a GeoShape is passed it back
         result = deepcopy(shapely_geom)
@@ -26,4 +60,8 @@ def make_geoshape_from_geom(
     elif isinstance(shapely_geom, LinearRing):
         result = GeoLinearRing(shapely_geom, crs=crs)
     # TODO add multigeoms
+    else:
+        return TypeError(
+            f"No suitable Geo-equivalent exists for value of type {type(shapely_geom)}"
+        )
     return result
